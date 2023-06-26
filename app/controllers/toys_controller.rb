ec5f483d617c1261682319155faa85eebb3e943c
sandra <nyamweasandra@gmail.com>
2023-06-26 13:13:45 +0300

    done

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,31 +1,20 @@
 class ToysController < ApplicationController
   wrap_parameters format: []
 
-  def index
-    toys = Toy.all
-    render json: toys
-  end
-
   def create
-    toy = Toys.create(toy_params)
-    render json: toy, status: :created
+    toy = Toy.new(toy_params)
+    if toy.save
+      render json: toy, status: :created
+    else
+      render json: { error: toy.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
-  def update
-    toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
-  end
-
-  def destroy
-    toy = Toy.find_by(id: params[:id])
-    toy.destroy
-    head :no_content
-  end
+  # Other actions...
 
   private
   
   def toy_params
     params.permit(:name, :image, :likes)
   end
-
 end
--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,2 +1,5 @@
 class Toy < ApplicationRecord
+    validates :name, presence: true
+    validates :likes, numericality: { greater_than_or_equal_to: 0 }
+  
 end
